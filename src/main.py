2763b6d755e1e8569323b0dd31f554a861b0fc3c
Matheus Braga <matheuskbraga9@gmail>
2025-07-11 16:57:10 -0300

    Treino da OpenCV

--- src/main.py
+++ src/main.py
@@ -1,26 +1,34 @@
 import cv2
 
-video = cv2.VideoCapture(0)
 
-if not video.isOpened():
-    print("Não foi possível acessar a câmera.")
-    exit()
+# Leitura da imagem
+img = cv2.imread('roboedu.png')
 
-while True:
-    ret, frame = video.read()
-    if not ret:
-        print("Falha ao capturar o frame.")
-        break
+if img is None:
+    raise FloatingPointError("Não foi possível carregar a imagem PNG")
 
-    cv2.imshow('Video ao Vivo', frame)
 
-    tecla = cv2.waitKey(1)
-    if tecla == ord('s'):
-        cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('frame_cinza.jpg', cinza)
-        print("Frame salvo!")
-    elif tecla == ord('q'):
-        break
+# Conversão de escala para cinza
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-video.release()
+
+# Desenho de triângulos coloridos em regiões fixas
+# Cada retângulo é definido por (x1, y1) conto superior esquerdo e (x2, y2) canto inferior direito
+rects = [
+    ((50,  50),  (200, 200), (0,   0, 255)),  # Vermelho
+    ((250, 50),  (400, 200), (0, 255,   0)),  # Verde
+    ((50, 250),  (200, 400), (255,   0,   0)) # Azul
+]
+
+
+# Preparar uma cópia colorida da imagem em tons de cinza
+output = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
+for (pt1, pt2, color) in rects:
+    cv2.rectangle(output, pt1, pt2, color, thickness=3)
+
+
+# Resultado
+cv2.imshow("Resultado", output)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
